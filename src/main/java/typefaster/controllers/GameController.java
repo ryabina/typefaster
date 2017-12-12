4804e85d124fafac527d6208768a5a42da86c9e2
Albert Fazullin <albert.fazullin@flatstack.com>
2017-12-12 10:52:45 +0300

    added annotation to controller

--- src/main/java/typefaster/controllers/GameController.java
+++ src/main/java/typefaster/controllers/GameController.java
@@ -1,17 +1,23 @@
 package typefaster.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import typefaster.models.Game;
 import typefaster.models.IGame;
 
+@Controller
 public class GameController implements IGameController {
     IGame game = new Game();
     @Override
+    @RequestMapping(path = "/game", method = RequestMethod.POST)
     public String startGame() {
         game.setStartTime();
         return "Game started!";
     }
 
     @Override
+    @RequestMapping(path = "/game", method = RequestMethod.PATCH)
     public String endGame(String text) {
         game.setText(text);
         game.setEndTime();
@@ -19,6 +25,7 @@ public class GameController implements IGameController {
     }
 
     @Override
+    @RequestMapping(path = "/game", method = RequestMethod.GET)
     public String showVelocity(){
         try{
             game.getVelocity();
